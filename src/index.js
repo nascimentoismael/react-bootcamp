5131169b8f1f4228cd1f946d9e6518cba974af26
Ismael Nascimento <55846164+nascimentoismael@users.noreply.github.com>
2022-08-15 19:13:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Feed from './Feed';
 
-const App = () => {
-    return <p>Olá Mundo!</p>
-}
+// const data = new Date();
+// data.getHours()
+
+// const App = () => {
+//   const mensagem = 'Olá Mundo';  
+//   return <p>{mensagem}!</p>
+// }
+
+// const App = () => {
+//   if (data.getHours() >0 && data.getHours() < 12) 
+//     return <h3>Bom dia!</h3>
+//   else if (data.getHours() > 12 && data.getHours() <18)
+//     return <h3>Boa tarde!</h3>
+//   else
+//     return <h3>Boa noite! São {data.getHours()} horas</h3>
+// }
+
+// const App = function() {
+//   return(
+//     <div>
+//       <label htmlFor='nome'>Nome: </label>
+//       <input id='nome'/>
+//     </div>
+//   );
+// };
+
+const App = function() {
+  return <h1><Feed /></h1>;
+  
+};
 
 ReactDom.render(<App />, document.querySelector("#root"));
