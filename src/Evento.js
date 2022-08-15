c85638b50e2fa31d3376098e8f5c0884746c1ffb
Ismael Nascimento <55846164+nascimentoismael@users.noreply.github.com>
2022-08-15 20:17:03 -0300

    Update Evento.js

--- src/Evento.js
+++ src/Evento.js
@@ -1,33 +1,37 @@
-import { faker } from '@faker-js/faker';
+import React from "react";
 
-const Evento = (props) => {
-    console.log(props);
-    
-    return(
-        <div className="event">
-                <div class="label">
-                    <img src={props.image}/>
-                </div>
-                <div className="content">
-                    <div className="summary">
-                        <a className="user">
-                        {props.usuario}
-                        </a> 
-                        <strong>
-                            {props.action}
-                        </strong>
-                        <div className="date">
-                        {props.date}
+class Evento extends React.Component {
+    // constructor(props){
+    //     super(props);
+    // }
+
+    render(){
+        return(
+            <div className="event">
+                    <div class="label">
+                        <img src={this.props.image} alt="imagem"/>
+                    </div>
+                    <div className="content">
+                        <div className="summary">
+                            <a className="user">
+                            {this.props.usuario}
+                            </a> 
+                            <strong>
+                                {this.props.action}
+                            </strong>
+                            <div className="date">
+                            {this.props.date}
+                            </div>
+                        </div>
+                        <div className="meta">
+                            <a className="like" href={this.props.link}>
+                            <i className="like icon"></i> {this.props.likes}
+                            </a>
                         </div>
                     </div>
-                    <div className="meta">
-                        <a className="like">
-                        <i className="like icon"></i> {props.likes}
-                        </a>
-                    </div>
                 </div>
-            </div>
-    );
+        );
+    }
 };
 
 export default Evento;
